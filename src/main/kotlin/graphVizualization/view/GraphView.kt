c9248864700e589b200acba5d1d4349d30a1a7f3
Stepan <st085873@gmail.com>
2021-05-07 15:54:31 +0300

    Big change of GraphView

--- src/main/kotlin/graphVizualization/view/GraphView.kt
+++ src/main/kotlin/graphVizualization/view/GraphView.kt
@@ -2,14 +2,17 @@ package graphVizualization.view
 
 import graphVizualization.controller.GraphController
 import graphVizualization.model.Graph
-import javafx.scene.layout.Pane
-import tornadofx.add
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
+import javafx.scene.Parent
+import tornadofx.*
 import java.lang.IllegalArgumentException
 
 class GraphView(
-    val graph: Graph
-) : Pane() {
-    //TODO проверить с lazy
+    var name: StringProperty = SimpleStringProperty("Undefined")
+) : View() {
+
+    val graph: Graph = Graph.RandomGraph()
     var vertices = graph.vertices().associateWith {
             VertexView(it)
         } as MutableMap
@@ -20,22 +23,48 @@ class GraphView(
         } as MutableMap
     val controller: GraphController = GraphController(this)
 
-    init {
+    override val root: Parent = pane {
         edges.values.forEach { e ->
             add(e)
+            setHandlersOnEdge(e)
         }
         vertices.values.forEach { v ->
             add(v)
             add(v.label)
+            setHandlersOnVertex(v)
         }
-        this.setOnScroll {
+    }
+
+    init {
+        root.setOnScroll {
             controller.onScroll(it, this)
         }
-        this.setOnMousePressed {
+        root.setOnMousePressed {
             controller.onMousePressed(it, this)
         }
-        this.setOnMouseDragged {
+        root.setOnMouseDragged {
             controller.onMouseDragged(it, this)
+        }
+    }
+
+    fun setHandlersOnVertex(vertexView: VertexView) {
+        vertexView.setOnMousePressed {
+            controller.onPressVertex(it, vertexView)
+        }
+        vertexView.setOnMouseDragged {
+            vertexView.controller.onDrag(it, vertexView)
+        }
+        vertexView.setOnMouseEntered {
+            vertexView.controller.onMouseEntered(vertexView)
+        }
+        vertexView.setOnMouseExited {
+            vertexView.controller.onMouseExited(vertexView)
+        }
+    }
+
+    fun setHandlersOnEdge(edgeView: EdgeView) {
+        edgeView.setOnMousePressed {
+            if(it.isAltDown) openInternalWindow(edgeView.weightEditor)
         }
     }
 }
