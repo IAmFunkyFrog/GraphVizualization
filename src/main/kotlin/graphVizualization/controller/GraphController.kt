c553f60e21670917df0fb96e7caf1ed5c9ec7a4a
Stepan <st085873@gmail.com>
2021-05-07 15:53:56 +0300

    Added createEdge and createVertex methods

--- src/main/kotlin/graphVizualization/controller/GraphController.kt
+++ src/main/kotlin/graphVizualization/controller/GraphController.kt
@@ -1,14 +1,14 @@
 package graphVizualization.controller
 
 import Vertex
-import graphVizualization.model.Graph
+import graphVizualization.model.Edge
+import graphVizualization.view.EdgeView
 import graphVizualization.view.GraphView
 import graphVizualization.view.VertexView
 import javafx.concurrent.ScheduledService
 import javafx.concurrent.Task
 import javafx.event.EventHandler
 import javafx.geometry.Point2D
-import javafx.scene.Node
 import javafx.scene.input.*
 import javafx.util.Duration
 import tornadofx.Controller
@@ -42,7 +42,7 @@ class ForceAtlas2Service(
 }
 
 class GraphController(
-    private val graphView: GraphView = GraphView(Graph.EmptyGraph())
+    private val graphView: GraphView
 ): Controller() {
 
     var forceAtlas2 = ForceAtlas2(graphView)
@@ -57,39 +57,68 @@ class GraphController(
         forceAtlas2Service.start()
     }
 
-    fun createVertex(vertexValue: String) {
-        val newVertex = Vertex(vertexValue)
-        graphView.graph.addVertex(newVertex)
-        graphView.vertices[newVertex] = VertexView(newVertex).also {
-            graphView.add(it as Node)
-            graphView.add(it.label as Node)
-        }
-        forceAtlas2 = ForceAtlas2(graphView)
-    }
-
     private var oldMousePos = Point2D(0.0, 0.0)
 
     fun onScroll(e: ScrollEvent, graphView: GraphView) {
-        if(!e.isControlDown) return
+        if (!e.isControlDown) return
 
         val delta = e.deltaY / 1000
-        if(graphView.scaleX + delta >= 0) graphView.scaleX += delta
-        if(graphView.scaleY + delta >= 0) graphView.scaleY += delta
+        if (graphView.root.scaleX + delta >= 0) graphView.root.scaleX += delta
+        if (graphView.root.scaleY + delta >= 0) graphView.root.scaleY += delta
     }
 
     fun onMousePressed(e: MouseEvent, graphView: GraphView) {
-        if(!e.isControlDown) return
+        if (!e.isControlDown) return
 
-        oldMousePos = graphView.localToParent(e.x, e.y)
+        oldMousePos = graphView.root.localToParent(e.x, e.y)
     }
 
     fun onMouseDragged(e: MouseEvent, graphView: GraphView) {
-        if(!e.isControlDown) return
+        if (!e.isControlDown) return
 
-        val mousePos = graphView.localToParent(e.x, e.y)
-        graphView.translateX += mousePos.x - oldMousePos.x
-        graphView.translateY += mousePos.y - oldMousePos.y
+        val mousePos = graphView.root.localToParent(e.x, e.y)
+        graphView.root.translateX += mousePos.x - oldMousePos.x
+        graphView.root.translateY += mousePos.y - oldMousePos.y
 
         oldMousePos = mousePos
+    }
+
+    var pressedVertex: VertexView? = null
+
+    fun onPressVertex(e: MouseEvent, vertexView: VertexView) {
+        if (!e.isSecondaryButtonDown) {
+            pressedVertex = null
+            return
+        }
+
+        if(pressedVertex == null) pressedVertex = vertexView
+        else pressedVertex?.let { createEdge(it, vertexView) }
+    }
+
+    fun createVertex(vertexValue: String) {
+        val newVertex = Vertex(vertexValue)
+        if(graphView.graph.addVertex(newVertex) != null) {
+            graphView.vertices[newVertex] = VertexView(newVertex).also {
+                graphView.root.add(it)
+                graphView.root.add(it.label)
+                graphView.setHandlersOnVertex(it)
+            }
+            forceAtlas2 = ForceAtlas2(graphView)
+        }
+    }
+
+    fun createEdge(vertexView1: VertexView, vertexView2: VertexView) {
+        if(vertexView1 == vertexView2) return
+        //TODO добавить возможность создавать ребра с определенным весом или хотя бы изменять вес ребра
+        val newEdge = Edge(vertexView1.vertex, vertexView2.vertex, 1.0)
+        if(graphView.graph.addEdge(newEdge) != null) {
+            graphView.edges[newEdge] = EdgeView(newEdge, vertexView1, vertexView2).also {
+                graphView.root.add(it)
+                graphView.setHandlersOnEdge(it)
+                //TODO разобраться с костылем
+                for((_, vView) in graphView.vertices) vView.toFront()
+            }
+            forceAtlas2 = ForceAtlas2(graphView)
+        }
     }
 }
