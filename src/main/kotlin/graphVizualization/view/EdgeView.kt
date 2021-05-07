3b0c0fc61eba344b9bd700b7f8aad34505551b20
Stepan <st085873@gmail.com>
2021-05-07 15:49:46 +0300

    Added WeightEditor to EdgeView

--- src/main/kotlin/graphVizualization/view/EdgeView.kt
+++ src/main/kotlin/graphVizualization/view/EdgeView.kt
@@ -1,9 +1,10 @@
 package graphVizualization.view
 
 import graphVizualization.model.Edge
-import javafx.scene.paint.Color
-import javafx.scene.paint.Paint
+import javafx.scene.Parent
+import javafx.scene.control.Label
 import javafx.scene.shape.Line
+import tornadofx.*
 
 class EdgeView(
     val edge: Edge,
@@ -16,6 +17,23 @@ class EdgeView(
         startYProperty().bind(vertexView1.centerYProperty())
         endXProperty().bind(vertexView2.centerXProperty())
         endYProperty().bind(vertexView2.centerYProperty())
+        strokeWidth = 3.0
     }
 
+    val weightEditor = WeightEditor()
+
+    inner class WeightEditor(): Fragment() {
+        override val root: Parent = pane {
+            vbox {
+                NumberField(edge.weight) {
+                    edge.weight = it
+                }.also {
+                    label("Weight") {
+                        labelFor = it
+                    }
+                    add(it)
+                }
+            }
+        }
+    }
 }
